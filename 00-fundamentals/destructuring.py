0a12ecd5346e1e898224b63aa0439acbfd4f132c
christopherkeibel <christopher.keibel@karakun.com>
2022-07-27 07:27:13 +0200

    use destructuring in for loop

---
+++ 00-fundamentals/destructuring.py
@@ -0,0 +1,10 @@
+
+if __name__=="__main__":
+    currencies = 0.8, 1.2
+    usd, eur = currencies
+    print(f"USD: {usd}\tEUR: {eur}")
+
+    friends = [("Anne", 34), ("Tom", 31), ("Mark", 25), ("Jen", 28)]
+
+    for name, age in friends:
+        print(f"{name} is {age} years old.")
