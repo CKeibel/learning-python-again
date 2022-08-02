eda777665ac5cb9d51d66a65820fd3d50b336ef9
christopherkeibel <christopher.keibel@karakun.com>
2022-08-02 14:31:40 +0200

    dunder methods/ magic methods

--- 01-OOP/dunder-methods.py
+++ 01-OOP/dunder-methods.py
@@ -6,12 +6,16 @@ class Garage:
     def __len__(self):
         return len(self.cars)
 
+    def __getitem__(self, index):
+        return self.cars[index]
+
 def main():
     my_garage = Garage()
     my_garage.cars.append("Audi")
     my_garage.cars.append("Ford")
 
     print(len(my_garage))
+    print(my_garage[1])
 
 
 if __name__ == "__main__":
